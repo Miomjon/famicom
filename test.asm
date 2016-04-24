53c5be9d3fa21f7b34ca10749cdfcf1b44b03735
Miomjon <kratosjon@gmail.com>
2016-04-24 16:35:47 +0200

    new stuff

--- test.asm
+++ test.asm
@@ -1,9 +1,24 @@
-	.bank 0
+	.bank 0 ; main
 	.org $8000
 
+PPU_Setup:
+
+	LDA #%10010011 ; PPU setup with mostly regular stuff
+	STA $2000
+	LDA #%00011111 : also mostly regular PPU config
+	STA $2001
+	RTS
 
 
 
+	.bank 1 ;interrupt stuff
+	.org $FFFA
 
-	.bank 1
-	.org $
+	.dw 0 ; apparently, location of NMI Interrupt
+	.dw Start ; when reset
+
+
+	.bank2 ; VROM
+	.org $0000
+
+	; .incbin "sprites.bin" example
